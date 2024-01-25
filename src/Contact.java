9a81adcf43f3ad80678b1eab6e974ff95e6abe94
Vedant Singh <singhvedant1701@gmail.com>
2024-01-25 11:04:06 +0530

    Use case 1: Contacts Creation Done

---
+++ src/Contact.java
@@ -0,0 +1,94 @@
+public class Contact {
+
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+
+
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook Program.");
+        Contact p1 = new Contact();
+        p1.setFirstName("Vedant");
+        p1.setLastName("Singh");
+        p1.setPhoneNumber("9694018045");
+        p1.setCity("Panna");
+        p1.setEmail("singhvedant1701@gmail.com");
+        p1.setAddress("03 Bajarang Road, Panna");
+        p1.setState("Madhya Pradesh");
+        System.out.println(p1);
     }
 }

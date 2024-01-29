cb8105dfa9d0022a57bde9a9d25d6688bcf94b8b
Vedant Singh <singhvedant1701@gmail.com>
2024-01-29 10:36:58 +0530

    Use case 6: Multiple books

---
+++ src/AddressBooks.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddressBooks {
+    private final HashMap<String, AddressBook> books;
+
+    public AddressBooks() {
+        this.books = new HashMap<>();
+    }
+
+    public void addBook(String bookName) {
+        AddressBook book = new AddressBook();
+        books.put(bookName, book);
+    }
+
+    public AddressBook getBook(String bookName) {
+        AddressBook addressBook = books.get(bookName);
+        if (addressBook == null) {
+            addressBook = new AddressBook();
+            books.put(bookName, addressBook);
+            return addressBook;
+        }
+        return addressBook;
+    }
+
+    public void removeBook(String bookName) {
+        AddressBook addressBook = books.get(bookName);
+        if (addressBook == null) {
+            System.out.println("Error : Wrong book name!");
+        } else {
+            books.remove(bookName);
+            System.out.println("Book removed!");
+        }
+    }
+
+    public void listBooks() {
+        for (Map.Entry<String, AddressBook> entry : books.entrySet()) {
+            System.out.println(entry.getKey());
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,77 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to AddressBook Program.");
-        AddressBook book1 = new AddressBook();
-
-        String input = "";
         Scanner scanner = new Scanner(System.in);
-        while (!input.equals("6")){
-            System.out.println("Please enter a command:");
-            System.out.println("1. Add a new contact");
-            System.out.println("2. Remove a contact");
-            System.out.println("3. List all contacts");
-            System.out.println("4. Edit a contact");
-            System.out.println("5. Delete a contact");
-            System.out.println("6. Exit");
-            System.out.print("Enter a command: ");
-            input = scanner.nextLine();
+        System.out.println("Welcome to AddressBook Program.");
+        AddressBooks app = new AddressBooks();
+        while (true) {
+            System.out.println("Address books in the database:");
+            System.out.println("--------------------------------");
+            app.listBooks();
+            System.out.println("--------------------------------");
+            System.out.println("Write the name of address book to open or name a new book to create:");
+            String bookName = scanner.nextLine();
+            AddressBook book = app.getBook(bookName);
 
-            switch (input) {
-                case "1":
-                    book1.addContact();
-                    break;
-                case "2":
-                    System.out.print("Enter Contact ID: ");
-                    int contactId = Integer.parseInt(scanner.nextLine());
-                    book1.removeContact(contactId);
-                    break;
-                case "3":
-                    book1.listContacts();
-                    break;
-                case "4":
-                    System.out.print("Enter First Name: ");
-                    String firstName = scanner.nextLine();
-                    System.out.print("Enter Last Name: ");
-                    String lastName = scanner.nextLine();
-                    book1.editContact(firstName, lastName);
-                    break;
-                case "5":
-                    System.out.print("Enter First Name: ");
-                    String firstName1 = scanner.nextLine();
-                    System.out.print("Enter Last Name: ");
-                    String lastName1 = scanner.nextLine();
-                    book1.deleteContact(firstName1, lastName1);
-                    break;
-                case "6":
-                    break;
-                default:
-                    System.out.println("Invalid command.");
-                    break;
+            String input = "";
+            while (!input.equals("6")) {
+                System.out.println("Please enter a command:");
+                System.out.println("1. Add a new contact");
+                System.out.println("2. Remove a contact");
+                System.out.println("3. List all contacts");
+                System.out.println("4. Edit a contact");
+                System.out.println("5. Delete a contact");
+                System.out.println("6. Exit the book");
+                System.out.println("7. Delete the book");
+                System.out.print("Enter a command: ");
+                input = scanner.nextLine();
+
+                switch (input) {
+                    case "1":
+                        book.addContact();
+                        break;
+                    case "2":
+                        System.out.print("Enter Contact ID: ");
+                        int contactId = Integer.parseInt(scanner.nextLine());
+                        book.removeContact(contactId);
+                        break;
+                    case "3":
+                        book.listContacts();
+                        break;
+                    case "4":
+                        System.out.print("Enter First Name: ");
+                        String firstName = scanner.nextLine();
+                        System.out.print("Enter Last Name: ");
+                        String lastName = scanner.nextLine();
+                        book.editContact(firstName, lastName);
+                        break;
+                    case "5":
+                        System.out.print("Enter First Name: ");
+                        String firstName1 = scanner.nextLine();
+                        System.out.print("Enter Last Name: ");
+                        String lastName1 = scanner.nextLine();
+                        book.deleteContact(firstName1, lastName1);
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        System.out.println("Enter Book name to delete : " + bookName);
+                        String bookName1 = scanner.nextLine();
+                        if (Objects.equals(bookName1, bookName)) {
+                            app.removeBook(bookName1);
+                            input = "6";
+                            System.out.println("Book deleted : " + bookName);
+                        }
+                        else {
+                            System.out.println("Book not deleted.");
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid command.");
+                        break;
+                }
             }
         }
     }

8b6c369e5a83c0b4ef0f299df476766537ffbfcc
Vedant Singh <singhvedant1701@gmail.com>
2024-02-16 15:25:54 +0530

    Use Case 10 : SQL - JDBC

---
+++ src/AddressBookManager.java
@@ -0,0 +1,76 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AddressBookManager {
+    private String jdbcURL = "jdbc:mysql://localhost:3306/AddressBookDB";
+    private String jdbcUsername = "root";
+    private String jdbcPassword = "root-user";
+
+    // SQL queries
+    private static final String INSERT_ADDRESSBOOK_SQL = "INSERT INTO ADDRESSBOOKS (BookName) VALUES (?, 'Address-book made by admin');";
+    private static final String INSERT_CONTACTS_SQL = "INSERT INTO Contacts (FirstName, LastName, PhoneNumber, Email, Address, AddressBookName) VALUES (?, ?, ?, ?, ?, ?);";
+
+    protected Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void insertAddressBook(Contact contact, String bookName) {
+        try (
+                Connection connection = getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(INSERT_ADDRESSBOOK_SQL)
+        ) {
+            preparedStatement.setString(1, bookName);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    public boolean insertContact(Contact contact, String bookName) {
+        try {
+            Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CONTACTS_SQL);
+            preparedStatement.setString(1, contact.getFirstName());
+            preparedStatement.setString(2, contact.getLastName());
+            preparedStatement.setString(3, contact.getPhoneNumber());
+            preparedStatement.setString(4, contact.getEmail());
+            preparedStatement.setString(5, contact.getAddress());
+            preparedStatement.setString(6, bookName);
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            printSQLException(e);
+            return false;
+        }
+    }
+    public boolean checkContactExist(Contact contact, String folderName) {
+        return false;
+    }
+    private void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+
+}
--- src/Contact.java
+++ src/Contact.java
@@ -38,6 +38,19 @@ public class Contact {
     public String getLastName() {
         return lastName;
     }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
     private void createContact() throws InputMismatchException {
         System.out.println("Enter Contact Details:");
 
@@ -124,23 +137,38 @@ public class Contact {
     }
 
 
-    public boolean save(Contact contact, String bookName) {
-        try  {
-            Path path = Paths.get("contacts/"+bookName+"/");
-            if (!Files.exists(path)) {
-                try {
-                    Files.createDirectories(path);
-                } catch (IOException e) {
-                    e.fillInStackTrace();
-                }
-            }
-            BufferedWriter writer = new BufferedWriter(new FileWriter(path+"/"+contact.getFirstName()+contact.getLastName()+".txt"));
-            writer.write(contact.toString());
-            writer.close();
-            return true;
-        } catch (IOException e) {
-            e.fillInStackTrace();
-            return false;
+    public boolean saveNewContact(Contact contact, String bookName) {
+//        try  {
+//            Path path = Paths.get("contacts/"+bookName+"/");
+//            if (!Files.exists(path)) {
+//                try {
+//                    Files.createDirectories(path);
+//                } catch (IOException e) {
+//                    e.fillInStackTrace();
+//                }
+//            }
+//            BufferedWriter writer = new BufferedWriter(new FileWriter(path+"/"+contact.getFirstName()+contact.getLastName()+".txt"));
+//            writer.write(contact.toString());
+//            writer.close();
+//            return true;
+//        } catch (IOException e) {
+//            e.fillInStackTrace();
+//            return false;
+//        }
+
+        AddressBookManager manager = new AddressBookManager();
+        return manager.insertContact(contact, bookName);
+    }
+    public boolean editContact(Contact value, String folderName) {
+        return false;
+    }
+    public boolean save(Contact contact, String folderName) {
+        AddressBookManager manager = new AddressBookManager();
+        boolean contactAlreadyExist = manager.checkContactExist(contact, folderName);
+        if (contactAlreadyExist) {
+            return saveNewContact(contact, folderName);
+        } else {
+            return editContact(contact, folderName);
         }
     }
 
